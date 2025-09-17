7abea03a9ffd475717b6d1329c6cf4bbef4135a8
Etiketaa <francoparedes1992@gmail.com>
2025-09-17 18:41:26 -0300

    Implement hamburger menu functionality

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,12 @@
 console.log('main.js is loading!');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger-menu');
+    const nav = document.querySelector('#main-nav');
+
+    if (hamburger && nav) {
+        hamburger.addEventListener('click', () => {
+            nav.classList.toggle('nav-active');
+        });
+    }
+});
